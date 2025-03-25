58b2defe0fbe7a06a9e5e77b823cad88b29b1b4c
MatthTsx <shakii876954@gmail.com>
2025-03-25 18:48:33 -0300

    [feat]:<Calculus intregrated>

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from PIL import Image, ImageColor
 import os
-from storedData import startingPoints
+from storedData import Data
 import math
 
 root = os.getcwd()
@@ -9,7 +9,8 @@ root = os.getcwd()
 fileName = "main1"
 fileType = ".jpg"
 
-startedPoint = startingPoints["Graph2"]
+coords, points, zero, startingPoint = Data["Graph2"].values()
+
 
 imageGraph = Image.open(root + "/ImagesBase/" + fileName + fileType)
 
@@ -17,25 +18,70 @@ width, height = imageGraph.size
 
 
 def getCoordinates():
-    for i in range(width):
-        for j in range(height):
-            pixel = imageGraph.getpixel((i,j))
-            if pixel[0] > 240 and pixel[1] < 50:
-                imageGraph.putpixel(xy=(i,j), value=(0,255,0))
-                print(pixel, i, j)
-                break
+    print(points)
+    for i in points:
+        imageGraph.putpixel((coords[0], i[1]), (0,255,0))
+        imageGraph.putpixel((coords[0]+1, i[1]), (0,255,0))
+        imageGraph.putpixel((coords[0]-1, i[1]), (0,255,0))
 
 
 # Its easier to just make the program count the pixels from top to bottom for each division
 # I can aussi make the divisions a user's selection or a video from 0 divisions to full
 
-divisions = 20
+divisions = 200
 
 def ForEveryPixel():
-    for i in range(startedPoint[0],width, divisions):
-        for j in range(startedPoint[1], height):
-            imageGraph.putpixel((i,j), (0,255,0))
-
+    ant_point = startingPoint
+    ant_y = "undefined"
+    
+    for i in range(coords[0], coords[2]+1, int(math.floor((coords[2]-coords[0])/divisions)) ):
+        y = 0
+        count = 0
+        for j in range(coords[1], coords[3]):
+            pixel = imageGraph.getpixel((i,j))
+            
+            if pixel[2] > 195 and pixel[0] + pixel[1] < 300:
+                y += j
+                count += 1
+                imageGraph.putpixel((i,j), (0,255,0))
+        
+        if not count:
+            continue
+        y = int(math.floor(y/count))
+        
+        for j in range(1, abs(zero - y)):
+            imageGraph.putpixel((i-1,zero + j * int(abs(zero-y)/(zero-y)) * -1 ), (0,0,0))
+            imageGraph.putpixel((i,zero + j * int(abs(zero-y)/(zero-y)) * -1 ), (0,0,0))
+            imageGraph.putpixel((i+1,zero + j * int(abs(zero-y)/(zero-y)) * -1 ), (0,0,0))
+    
+        imageGraph.putpixel(
+            (i, int(math.floor(y/count))),
+            (255,0,0)
+        )
+        y = zero - y
+        
+        if ant_y == "undefined":
+            ant_y = y
+            continue
+        
+        if abs(ant_y)/ant_y != abs(y)/y:
+            ant_y = 0
+        
+        
+        height = math.floor(abs(coords[0]-coords[2])/divisions)/(coords[2]-coords[0])*10
+        area = (ant_y+y)*height/2
+        print("para: ", ant_y, y, height)
+        print(area, (ant_y+y), y)
+        
+        area = int(math.floor(area))
+        ant_point = ant_point + area *-1
+        for k in range(-5,5):
+            for l in range(-5,5):
+                imageGraph.putpixel((i+k, ant_point+l), (255,0,0))
+        ant_y = y
+        
+        
+        
 ForEveryPixel()
 # getCoordinates()
 imageGraph.show()
--- storedData.py
+++ storedData.py
@@ -1,6 +1,19 @@
 
 
 # TODO: determine the limits (max values) of x and y
-startingPoints = {
-    "Graph2": (163, 178)
+Data = {
+    "Graph2": {
+        "Points" :(163*2, 178*2, 2804, 1780-4),
+        "CoordsValues": [
+            (2, 178*2),
+            (1, 600-8),
+            (0, 827),
+            (-1, 950 + 113),
+            (-2, 1300-2),
+            (-3, 1533),
+            (-4, 1780-12),
+        ],
+        "MidCoord": 827,
+        "StartingPoint": 178*2
+    }
 }
